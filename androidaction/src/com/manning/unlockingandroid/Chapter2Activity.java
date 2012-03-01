d1c24ff819453270528edc1c3f93014d85d9ea6d
Alex Kilbo <akilbo@gmail.com>
2012-03-01 13:03:17 -0800

    added android tutorials and tests

---
+++ androidaction/src/com/manning/unlockingandroid/Chapter2Activity.java
@@ -0,0 +1,13 @@
+package com.manning.unlockingandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Chapter2Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
