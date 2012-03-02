ebe17dcbb99e36ef84143fc833abe368e4ab4e7a
Alex Kilbo <akilbo@gmail.com>
2012-03-01 18:01:01 -0800

    half of chapter 2 done

--- androidaction/src/com/manning/unlockingandroid/Chapter2Activity.java
+++ androidaction/src/com/manning/unlockingandroid/Chapter2Activity.java
@@ -1,13 +1,19 @@
 package com.manning.unlockingandroid;
 
 import android.app.Activity;
-import android.os.Bundle;
+import android.os.Bundle; 
+import android.widget.EditText;
+import android.widget.TextView;
+import com.manning.unlockingandroid.R;
 
 public class Chapter2Activity extends Activity {
+	public static final String tag = "Chapter2";
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.main);
+        	final EditText mealpricefield = (EditText) findViewById (R.id.mealprice); 
+        	final TextView answerfield = (TextView) findViewById (R.id.answer);
     }
 }
