f2196302108b82e0c120ac6e6752feb7e6c685e8
Alex Kilbo <akilbo@gmail.com>
2012-03-10 21:52:10 -0800

    added to activity of ch3

--- Ch3/src/com/manning/ch3/ReviewCriteria.java
+++ Ch3/src/com/manning/ch3/ReviewCriteria.java
@@ -1,8 +1,12 @@
-package com.manning.ch3;
+package com.manning.ch3;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -11,7 +15,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 public class ReviewCriteria extends Activity {
-	public static final int MENU_GETS_REVIEWS = Menu.FIRST;
+	public static final int MENU_GET_REVIEWS = Menu.FIRST;
 	private Spinner cuisine;
 	private Button grabReviews;
 	private EditText location;
@@ -31,6 +35,49 @@ public class ReviewCriteria extends Activity {
         		handleGetReviews();
         	}
         });
-        
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+    	super.onCreateOptionsMenu(menu);
+    	menu.add(0,ReviewCriteria.MENU_GET_REVIEWS, 0, R.string.menu_get_reviews).setIcon(android.R.drawable.ic_menu_more);
+    	return true;
+    }
+    
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item){
+    	switch(item.getItemId()){
+    	case MENU_GET_REVIEWS:
+    		handleGetReviews();
+    		return true;
+    	}
+    return super.onMenuItemSelected(featureId, item);
+    }
+    private void handleGEtReviews(){
+    	if(!validate()){
+    		return;
+    	}
+    	ResturantFinderApplication application = (ResturantFinderApplication) getApplication();
+    	application.setReviewCriteriaCuisine(this.cuisine.getSelectedItem().toString());
+    	application.setReviewCriteriaLocation(this.location.getText().toString());
+    	Intent intent = new Intent(Constants.INTENT_ACTION_VIEW_LIST);
+    	startActivity(intent);
+    }
+    private boolean validate(){
+    	boolean valid = true;
+    	StringBuilder validationText = new StringBuilder();
+    	if ((this.location.getText()== null) || this.location.getText().toString().equals("")){
+    		validationText.append(getResources().getString(R.string.location_not_supplied_message));
+    		valid = false;
+    	}
+    	if(!valid){
+    		new AlertDialog.Builder(this).setTitle(getResources().getString(R.string.alert_label)).setMessage(validationText.toString()).setPositiveButton("Continue", new android.content.DialogInterface.OnClickListener() {
+    			
+				
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO Auto-generated method stub
+					
+				}
+			})
+    		}
     }
 }
